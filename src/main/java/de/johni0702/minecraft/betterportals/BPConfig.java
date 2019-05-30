aff8f115ebb05ac7c1d0d3827744e529b562d061
Jonas Herzig <me@johni0702.de>
2019-05-30 14:30:23 +0200

    Convert BPConfig to Java (fixes #7)

--- src/main/java/de/johni0702/minecraft/betterportals/BPConfig.java
+++ src/main/java/de/johni0702/minecraft/betterportals/BPConfig.java
@@ -1,41 +1,36 @@
-package de.johni0702.minecraft.betterportals
+package de.johni0702.minecraft.betterportals;
 
-import net.minecraftforge.common.config.Config
+import net.minecraftforge.common.config.Config;
 
-// TODO need some postprocessing in gradle build to annotate Kotlin's INSTANCE field with @Config.Ignore
+import static de.johni0702.minecraft.betterportals.BetterPortalsModKt.MOD_ID;
+
 @Config(modid = MOD_ID)
-object BPConfig {
-    @JvmField
+public class BPConfig {
     @Config.RequiresMcRestart
     @Config.Name("Enable Nether Portals")
     @Config.Comment("Whether to replace vanilla nether portals with BetterPortals ones.")
-    var enableNetherPortals: Boolean = true
+    public static boolean enableNetherPortals = true;
 
-    @JvmField
     @Config.RequiresMcRestart
     @Config.Name("Enable End Portals")
     @Config.Comment("Whether to replace vanilla end portals with BetterPortals ones.")
-    var enableEndPortals: Boolean = true
+    public static boolean enableEndPortals = true;
 
-    @JvmField
     @Config.Name("Prevent Fall Damage")
     @Config.Comment("Whether to prevent the next fall damage after a player has passed through a lying portal.")
-    var preventFallDamage: Boolean = true
+    public static boolean preventFallDamage = true;
 
-    @JvmField
     @Config.RequiresMcRestart
     @Config.Name("Enable TwilightForest Portals (Experimental!)")
     @Config.Comment("Whether to replace TwilightForest portals with BetterPortals ones. Experimental feature!")
-    var enableExperimentalTwilightForestPortals: Boolean = false
+    public static boolean enableExperimentalTwilightForestPortals = false;
 
-    @JvmField
     @Config.RequiresMcRestart
     @Config.Name("Replace Vanilla Chunk Render Dispatcher")
     @Config.Comment("Whether to replace the vanilla ChunkRenderDispatcher(s) with a view-aware one. For debugging only.")
-    var improvedChunkRenderDispatcher: Boolean = true
+    public static boolean improvedChunkRenderDispatcher = true;
 
-    @JvmField
     @Config.Name("See-through portals")
     @Config.Comment("Whether the other side of portals will be visible. Disabling will improve performance.")
-    var seeThroughPortals: Boolean = true
+    public static boolean seeThroughPortals = true;
 }
