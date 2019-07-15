11daa26432d560ce9388dac58d60141aeefc8da8
Jonas Herzig <me@johni0702.de>
2019-07-15 14:34:42 +0200

    Make the sound system portal-aware (fixes #115, see #107)

---
+++ src/portal/java/de/johni0702/minecraft/betterportals/impl/mixin/MixinSoundManager.java
@@ -0,0 +1,74 @@
+package de.johni0702.minecraft.betterportals.impl.mixin;
+
+import de.johni0702.minecraft.betterportals.impl.client.audio.PortalAwareSoundManager;
+import net.minecraft.client.audio.ISound;
+import net.minecraft.client.audio.ITickableSound;
+import net.minecraft.client.audio.SoundManager;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.Vec3d;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(SoundManager.class)
+public abstract class MixinSoundManager {
+    @Inject(method = "setListener(Lnet/minecraft/entity/Entity;F)V", at = @At("HEAD"), remap = false)
+    private void setListener(Entity entity, float partialTicks, CallbackInfo ci) {
+        if (entity != null) {
+            double x = entity.prevPosX + (entity.posX - entity.prevPosX) * partialTicks;
+            double y = entity.prevPosY + (entity.posY - entity.prevPosY) * partialTicks + entity.getEyeHeight();
+            double z = entity.prevPosZ + (entity.posZ - entity.prevPosZ) * partialTicks;
+            PortalAwareSoundManager.INSTANCE.setListenerPos(new Vec3d(x, y, z));
+        }
+    }
+
+    @Inject(method = "playDelayedSound", at = @At("HEAD"))
+    private void recordViewOnPlayDelayedSound(ISound sound, int delay, CallbackInfo ci) {
+        PortalAwareSoundManager.INSTANCE.recordView(sound);
+    }
+
+    @Inject(method = "playSound", at = @At("HEAD"))
+    private void recordViewOnPlaySound(ISound sound, CallbackInfo ci) {
+        PortalAwareSoundManager.INSTANCE.recordView(sound);
+    }
+
+    @Inject(method = "playSound", at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/audio/ISound;getVolume()F"
+    ))
+    private void considerPortalsBeforePlay(ISound sound, CallbackInfo ci) {
+        PortalAwareSoundManager.INSTANCE.beforePlay(sound);
+    }
+
+    @Redirect(method = "playSound", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/audio/ISound;getXPosF()F"))
+    private float getXPosFConsideringPortals$0(ISound sound) {
+        return (float) PortalAwareSoundManager.INSTANCE.getApparentPos(sound).x;
+    }
+
+    @Redirect(method = "playSound", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/audio/ISound;getYPosF()F"))
+    private float getYPosFConsideringPortals$0(ISound sound) {
+        return (float) PortalAwareSoundManager.INSTANCE.getApparentPos(sound).y;
+    }
+
+    @Redirect(method = "playSound", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/audio/ISound;getZPosF()F"))
+    private float getZPosFConsideringPortals$0(ISound sound) {
+        return (float) PortalAwareSoundManager.INSTANCE.getApparentPos(sound).z;
+    }
+
+    @Redirect(method = "updateAllSounds", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/audio/ITickableSound;getXPosF()F"))
+    private float getXPosFConsideringPortals$1(ITickableSound sound) {
+        return (float) PortalAwareSoundManager.INSTANCE.getApparentPos(sound).x;
+    }
+
+    @Redirect(method = "updateAllSounds", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/audio/ITickableSound;getYPosF()F"))
+    private float getYPosFConsideringPortals$1(ITickableSound sound) {
+        return (float) PortalAwareSoundManager.INSTANCE.getApparentPos(sound).y;
+    }
+
+    @Redirect(method = "updateAllSounds", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/audio/ITickableSound;getZPosF()F"))
+    private float getZPosFConsideringPortals$1(ITickableSound sound) {
+        return (float) PortalAwareSoundManager.INSTANCE.getApparentPos(sound).z;
+    }
+}
---
+++ src/portal/kotlin/de/johni0702/minecraft/betterportals/impl/client/audio/PortalAwareSoundManager.kt
@@ -0,0 +1,85 @@
+package de.johni0702.minecraft.betterportals.impl.client.audio
+
+import de.johni0702.minecraft.betterportals.common.*
+import de.johni0702.minecraft.view.client.ClientView
+import de.johni0702.minecraft.view.client.viewManager
+import net.minecraft.client.Minecraft
+import net.minecraft.client.audio.ISound
+import net.minecraft.util.math.Vec3d
+
+// Current shortcomings:
+// - If the sound can be heard through more than one portal, one is chosen at random
+// - Assumes portals to be stationary (sound system has only one listener, so this is hard to solve)
+// - Recursion never goes through the same view twice (e.g. no infinite echos)
+// - Passing through a portal will cause some sounds to suddenly switch direction. Really more of a shortcoming of
+//     non-euclidean spaces with the lack of physics-based sound in MC (and games in general), not sure if we can do
+//     anything about it.
+// - Doesn't currently bother with repositioning sounds after initial calculation anyway (should be fine for
+//     short sounds, don't care enough about long sounds)
+// - Clamping sounds to portals is currently done in the most simple way possible (and not accurate at all)
+// - New portals don't create new sound sources (limitation of sound system lack of source cloning)
+// - Closing portals doesn't stop sounds
+object PortalAwareSoundManager {
+    private val mc = Minecraft.getMinecraft()
+    private val viewForSound = mutableMapOf<ISound, ClientView>()
+    private val soundPaths = mutableMapOf<ISound, List<PortalAgent<*, *>>>()
+    var listenerPos = Vec3d(0.0, 0.0, 0.0)
+
+    fun recordView(sound: ISound) {
+        if (viewForSound[sound] != null) {
+            return
+        }
+        viewForSound[sound] = (mc.viewManager ?: return).activeView
+    }
+
+    fun beforePlay(sound: ISound) {
+        val view = viewForSound[sound] ?: return
+        val paths = findMainView(view, setOf(view)).toList()
+        if (paths.isEmpty()) return
+        soundPaths[sound] = paths.first()
+    }
+
+    private fun findMainView(from: ClientView, visited: Set<ClientView>): Set<List<PortalAgent<*, *>>> = if (from.isMainView) {
+        setOf(emptyList())
+    } else {
+        val portalManager = from.camera.world.portalManager
+        portalManager.loadedPortals.flatMapTo(mutableSetOf<List<PortalAgent<*, *>>>()) { portalAgent ->
+            val view = portalAgent.view ?: return@flatMapTo emptySet()
+            if (view in visited) {
+                emptySet()
+            } else {
+                findMainView(view, visited + setOf(view)).map { listOf(portalAgent) + it }
+            }
+        }
+    }
+
+    val ISound.apparentPos get() = calcApparentLocation(
+            Vec3d(xPosF.toDouble(), yPosF.toDouble(), zPosF.toDouble()),
+            listenerPos,
+            soundPaths[this] ?: emptyList()
+    )
+
+    private fun calcApparentLocation(pos: Vec3d, listener: Vec3d, portalStack: List<PortalAgent<*, *>>): Vec3d {
+        val portalAgent = portalStack.lastOrNull() ?: return pos
+        val portal = portalAgent.portal
+        val apparentPos = with(portal) {
+            calcApparentLocation(pos, listener.fromRemote().toLocal(), portalStack.dropLast(1)).fromLocal().toRemote()
+        }
+
+        // Clamp sound direction to portal surface (for sanity's sake, ignore the detailed bounding box)
+        val portalBox = portal.remoteBoundingBox
+        return if (portalBox.calculateIntercept(listener, apparentPos) != null) {
+            // Direct line of sight (disregarding frame), good enough
+            apparentPos
+        } else {
+            // The correct way would be minimizing the function which calculates the distance from sound to portal to
+            // listener with the additional constraint that the solution be on the portal surface.
+            // That's far too much work though, so we'll just assume that point to be in the center of the portal and
+            // hope no one notices.
+            val intersectionPoint = portalBox.center
+            val distanceFromPortal = apparentPos.distanceTo(intersectionPoint)
+            val direction = (intersectionPoint - listener).normalize()
+            intersectionPoint + direction * distanceFromPortal
+        }
+    }
+}
