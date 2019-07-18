48baccc8653c8aef1aa17c021f4fc4c04e1836b1
Jonas Herzig <me@johni0702.de>
2019-07-18 20:12:55 +0200

    Fix crash when music plays in Missing Mod error screen (fixes #142)
    Specifically this will return a dummy ViewAPI implementation
    when one is requested before the mod itself is loaded and when
    forge has an error to display.

--- src/api/kotlin/de/johni0702/minecraft/view/common/ViewAPI.kt
+++ src/api/kotlin/de/johni0702/minecraft/view/common/ViewAPI.kt
@@ -1,8 +1,19 @@
 package de.johni0702.minecraft.view.common
 
 import de.johni0702.minecraft.view.client.ClientViewAPI
+import de.johni0702.minecraft.view.client.ClientViewManager
+import de.johni0702.minecraft.view.client.render.RenderPass
+import de.johni0702.minecraft.view.client.render.RenderPassManager
+import de.johni0702.minecraft.view.server.ServerView
 import de.johni0702.minecraft.view.server.ServerViewAPI
+import de.johni0702.minecraft.view.server.ServerViewManager
+import net.minecraft.client.Minecraft
+import net.minecraft.entity.player.EntityPlayerMP
+import net.minecraft.network.NetHandlerPlayServer
+import net.minecraftforge.fml.client.FMLClientHandler
+import net.minecraftforge.fml.common.FMLCommonHandler
 import net.minecraftforge.fml.common.Loader
+import net.minecraftforge.fml.relauncher.Side
 
 /**
  * Entry point into the view API. See [ClientViewAPI] and [ServerViewAPI]
@@ -10,7 +21,16 @@ import net.minecraftforge.fml.common.Loader
 interface ViewAPI {
     companion object {
         @JvmStatic
-        val instance by lazy { Loader.instance().indexedModList["betterportals"]!!.mod as ViewAPI }
+        val instance by lazy {
+            val mod = Loader.instance().indexedModList["betterportals"]!!.mod
+            if (mod == null && FMLCommonHandler.instance().side == Side.CLIENT && FMLClientHandler.instance().hasError()) {
+                // Forge has probably aborted mod loading and may wish to show a user-friendly error GUI,
+                // so we need to return a dummy ViewAPI implementation as otherwise we would have to crash.
+                FailureFallbackViewAPI
+            } else {
+                mod as ViewAPI
+            }
+        }
     }
 
     /**
@@ -24,4 +44,25 @@ interface ViewAPI {
      * Not to be accessed from the client.
      */
     val server: ServerViewAPI
+}
+
+private object FailureFallbackViewAPI : ViewAPI {
+    override val client: ClientViewAPI by lazy { FailureFallbackClientViewAPI }
+    override val server: ServerViewAPI by lazy { FailureFallbackServerViewAPI }
+
+    private object FailureFallbackClientViewAPI : ClientViewAPI {
+        override fun getViewManager(minecraft: Minecraft): ClientViewManager? = null
+        override fun getRenderPassManager(minecraft: Minecraft): RenderPassManager = FailureFallbackRenderPassManager
+
+        private object FailureFallbackRenderPassManager : RenderPassManager {
+            override val root: RenderPass? = null
+            override val current: RenderPass? = null
+            override val previous: RenderPass? = null
+        }
+    }
+
+    object FailureFallbackServerViewAPI : ServerViewAPI {
+        override fun getViewManager(connection: NetHandlerPlayServer): ServerViewManager = throw UnsupportedOperationException()
+        override fun getView(player: EntityPlayerMP): ServerView? = throw UnsupportedOperationException()
+    }
 }
