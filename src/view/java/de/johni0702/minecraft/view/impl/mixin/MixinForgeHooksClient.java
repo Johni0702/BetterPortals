5807f500772044d11a73e363afed5ac68ddd3d25
Jonas Herzig <me@johni0702.de>
2019-09-07 18:21:52 +0200

    Fix Forge's sky color cache to work with multiple worlds (fixes #323)
    To reproduce the original issue, one must ensure that the cache
    from the two worlds collides, i.e. the x and z block coordinates of the
    camera must be the same in both worlds.
    Given the Aether is largely empty, there's a good chance a portal will
    generate at the same coordinates over there as it was in the overworld.

---
+++ src/view/java/de/johni0702/minecraft/view/impl/mixin/MixinForgeHooksClient.java
@@ -0,0 +1,42 @@
+package de.johni0702.minecraft.view.impl.mixin;
+
+import kotlin.Triple;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.client.ForgeHooksClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.WeakHashMap;
+
+@Mixin(value = ForgeHooksClient.class, remap = false)
+public abstract class MixinForgeHooksClient {
+
+    @Shadow private static boolean skyInit;
+    @Shadow private static int skyRGBMultiplier;
+    @Shadow private static int skyX;
+    @Shadow private static int skyZ;
+
+    private static WeakHashMap<World, Triple<Integer, Integer, Integer>> skyColorCache = new WeakHashMap<>();
+
+    @Inject(method = "getSkyBlendColour", at = @At("HEAD"))
+    private static void loadSkyColorCache(World world, BlockPos center, CallbackInfoReturnable<Integer> ci) {
+        Triple<Integer, Integer, Integer> cache = skyColorCache.get(world);
+        if (cache != null) {
+            skyInit = true;
+            skyX = cache.component1();
+            skyZ = cache.component2();
+            skyRGBMultiplier = cache.component3();
+        } else {
+            skyInit = false;
+        }
+    }
+
+    @Inject(method = "getSkyBlendColour", at = @At(value = "RETURN", ordinal = 1))
+    private static void storeSkyColorCache(World world, BlockPos center, CallbackInfoReturnable<Integer> ci) {
+        skyColorCache.put(world, new Triple<>(skyX, skyZ, skyRGBMultiplier));
+    }
+}
