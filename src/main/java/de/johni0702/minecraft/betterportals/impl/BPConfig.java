6813a3b9edd7109cb570b74280b1a2f9fec31601
Jonas Herzig <me@johni0702.de>
2019-09-25 17:41:03 +0200

    Clearly mark client-side settings as such (closes #360)

--- src/main/java/de/johni0702/minecraft/betterportals/impl/BPConfig.java
+++ src/main/java/de/johni0702/minecraft/betterportals/impl/BPConfig.java
@@ -52,16 +52,23 @@ public class BPConfig {
     }
 
     @Config.Name("Prevent Fall Damage")
-    @Config.Comment("Whether to prevent the next fall damage after a player has passed through a lying portal.")
+    @Config.Comment("Whether to prevent the next fall damage after a player has passed through a lying portal.\n" +
+            "\n" +
+            "Server-side setting."
+    )
     public static boolean preventFallDamage = true;
 
     @Config.Name("Sound through portals")
     @Config.Comment("Whether to redirect sounds in the remote world of a portal to originate from the direction of the portal." +
-            "Disabling will drop all sounds not originating in the primary world.")
+            "Disabling will drop all sounds not originating in the primary world.\n" +
+            "\n" +
+            "Client-side setting.")
     public static boolean soundThroughPortals = true;
 
     @Config.Name("See-through portals")
-    @Config.Comment("Whether the other side of portals will be visible. Disabling will improve performance.")
+    @Config.Comment("Whether the other side of portals will be visible. Disabling will improve performance.\n" +
+            "\n" +
+            "Client-side setting.")
     public static boolean seeThroughPortals = true;
 
     @Config.Name("Portals in portals limit")
@@ -73,7 +80,9 @@ public class BPConfig {
             "Etc.\n" +
             "\n" +
             "This only applies to portals which already have their remote world loaded.\n" +
-            "The recursion limit for loading of portals is a fixed 1.")
+            "The recursion limit for loading of portals is a fixed 1.\n" +
+            "\n" +
+            "Client-side setting.")
     @Config.RangeInt(min = 0)
     public static int recursionLimit = 2;
 
@@ -95,7 +104,7 @@ public class BPConfig {
     public static int enhancedThirdPartyTransferSeconds = 10;
 
     @Config.Name("Debug View")
-    @Config.Comment("Show debug view instead of composed view.")
+    @Config.Comment("Show debug view instead of composed view. Client-side setting.")
     public static boolean debugView = false;
 
     @SubscribeEvent
--- src/main/java/de/johni0702/minecraft/betterportals/impl/PortalConfig.java
+++ src/main/java/de/johni0702/minecraft/betterportals/impl/PortalConfig.java
@@ -5,13 +5,15 @@ import net.minecraftforge.common.config.Config;
 public class PortalConfig {
     @Config.RequiresMcRestart
     @Config.Name("Enable")
-    @Config.Comment("Whether to replace this kind of portal with BetterPortals ones.")
+    @Config.Comment("Whether to replace this kind of portal with BetterPortals ones. Needs to be set on the server and the client.")
     public boolean enabled = true;
 
     @Config.Name("Opacity")
     @Config.Comment("Determines the opacity of the original portal texture.\n" +
             "A value of 0 will not render the original texture at all.\n" +
-            "A value of 1 is maximally opaque, i.e. unchanged from the original value (the remote world will nevertheless be rendered).")
+            "A value of 1 is maximally opaque, i.e. unchanged from the original value (the remote world will nevertheless be rendered).\n" +
+            "\n" +
+            "Client-side setting.\n")
     @Config.RangeDouble(min = 0, max = 1)
     public double opacity = 0.5;
 
@@ -20,7 +22,9 @@ public class PortalConfig {
             "If this value is greater than the \"Minimal render distance\", that value is used instead.\n" +
             "\n" +
             "A value between 0 and 1 specifies a fraction of the overall render distance.\n" +
-            "A value equal or greater than 1 specifies an absolute distance in chunks.\n"
+            "A value equal or greater than 1 specifies an absolute distance in chunks.\n" +
+            "\n" +
+            "Client-side setting.\n"
     )
     @Config.RangeDouble(min = 0, max = 64)
     public double renderDistMin = 1.0;
@@ -29,18 +33,22 @@ public class PortalConfig {
     @Config.Comment("The distance at which portals will begin to render the remote world.\n" +
             "\n" +
             "A value between 0 and 1 specifies a fraction of the overall render distance.\n" +
-            "A value equal or greater than 1 specifies an absolute distance in chunks.\n"
+            "A value equal or greater than 1 specifies an absolute distance in chunks.\n" +
+            "\n" +
+            "Client-side setting.\n"
     )
     @Config.RangeDouble(min = 0, max = 64)
     public double renderDistMax = 3.00;
 
     @Config.Name("Size render distance multiplier")
     @Config.Comment("For portals which have a side length greater than this value, the \"Minimal render distance\" and \"Fully visible distance\" values\n will be doubled.\n" +
-     "If the side length is greater than twice this value, they will be tripled.\n" +
-     "Greater than thrice this value, they will be quadrupled.\n" +
-     "Etc.\n" +
-     "\n" +
-     "Render dist values between 0 and 1 will be scaled after they've been converted into absolute ones.\n"
+            "If the side length is greater than twice this value, they will be tripled.\n" +
+            "Greater than thrice this value, they will be quadrupled.\n" +
+            "Etc.\n" +
+            "\n" +
+            "Render dist values between 0 and 1 will be scaled after they've been converted into absolute ones.\n" +
+            "\n" +
+            "Client-side setting.\n"
     )
     public int renderDistSizeMultiplier = 10;
 }
