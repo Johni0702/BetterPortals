Johni0702/BetterPortals

commit 8ce765b64399ac30fd0b916df959b2c83761260b

[1.12] Workaround Mekanism overwriting the vanilla nether portal

src/vanilla/kotlin/de/johni0702/minecraft/betterportals/impl/vanilla/common/Extensions.kt
@@ -39,6 +39,7 @@ import net.minecraft.util.ResourceLocation
 //$$ import java.util.function.Supplier
 //#else
 import net.minecraft.tileentity.TileEntity
+import net.minecraftforge.fml.common.Loader
 import net.minecraftforge.fml.common.registry.EntityRegistry
 //#endif
 
@@ -53,6 +54,7 @@ interface BlockWithBPVersion {
 fun initVanilla(
         mod: Any,
         clientPreInit: (() -> Unit) -> Unit,
+        postInit: (() -> Unit) -> Unit,
         registerBlocks: (BlockRegistry.() -> Unit) -> Unit,
         registerTileEntities: (TileEntityTypeRegistry.() -> Unit) -> Unit,
         registerEntities: (EntityTypeRegistry.() -> Unit) -> Unit,
@@ -99,6 +101,23 @@ fun initVanilla(
     }
 
     registerBlocks {
+        //#if MC>=11400
+        if (enableNetherPortals && Loader.isModLoaded("mekanism")) {
+            // If we aren't doing it, Mekanism will (and break our stuff cause it overwrites important methods)
+            register(object : net.minecraft.block.BlockPortal() {
+                init {
+                    registryName = ResourceLocation("minecraft", "portal")
+                    setHardness(-1.0F)
+                    soundType = net.minecraft.block.SoundType.GLASS
+                    setLightLevel(0.75F)
+                    unlocalizedName = "portal"
+                }
+            })
+        }
+        //#endif
+    }
+
+    postInit {
         if (enableNetherPortals) {
             (Blocks.PORTAL as BlockWithBPVersion).enableBetterVersion(mod)
         }

src/main/kotlin/de/johni0702/minecraft/betterportals/impl/BetterPortalsMod.kt
@@ -126,6 +126,7 @@ internal class BetterPortalsMod: ViewAPI by ViewAPIImpl, BetterPortalsAPI by Bet
         initVanilla(
                 mod = this,
                 clientPreInit = { clientPreInitCallbacks.add(it) },
+                postInit = { commonPostInitCallbacks.add(it) },
                 registerBlocks = { registerBlockCallbacks.add(it) },
                 registerTileEntities = { registerTileEntitiesCallbacks.add(it) },
                 registerEntities = { registerEntitiesCallbacks.add(it) },

commit aa00a184428c25b9d0a77a52ac5a48e63ff1f8ad

Fix typo in preprocessor statement introduced in 8ce765b

src/vanilla/kotlin/de/johni0702/minecraft/betterportals/impl/vanilla/common/Extensions.kt
@@ -101,7 +101,7 @@ fun initVanilla(
     }
 
     registerBlocks {
-        //#if MC>=11400
+        //#if MC<11400
         if (enableNetherPortals && Loader.isModLoaded("mekanism")) {
             // If we aren't doing it, Mekanism will (and break our stuff cause it overwrites important methods)
             register(object : net.minecraft.block.BlockPortal() {