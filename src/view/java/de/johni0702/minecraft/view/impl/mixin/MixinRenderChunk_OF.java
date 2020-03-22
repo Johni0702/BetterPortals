e19a6b292ca36a51ceec65b21db1c50fab4a9d52
Jonas Herzig <me@johni0702.de>
2020-03-22 12:52:51 +0100

    Fix compilation of MixinRenderChunk_OF
    Turns out the workaround introduced in a951215 doesn't actually compile
    outside of Intellij.
    So, Shadow compiles but fails when applying, Accessor applies but fails
    when compiling.. Reflection it is then.

--- src/view/java/de/johni0702/minecraft/view/impl/mixin/MixinRenderChunk_OF.java
+++ src/view/java/de/johni0702/minecraft/view/impl/mixin/MixinRenderChunk_OF.java
@@ -4,21 +4,93 @@ import de.johni0702.minecraft.view.impl.compat.OFRenderChunk;
 import net.minecraft.client.renderer.chunk.RenderChunk;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.Shadow;
+
+//#if FABRIC>=1
+//$$ import java.lang.reflect.Field;
+//#endif
 
 @Mixin(RenderChunk.class)
 @SideOnly(Side.CLIENT)
-public interface MixinRenderChunk_OF extends OFRenderChunk {
+public abstract class MixinRenderChunk_OF implements OFRenderChunk {
+    // Optifabric adds these after Mixin parses the class meta, so mixin complains if we try to shadow them.
+    //#if FABRIC>=1
+    //$$ private ChunkRenderer[] _renderChunkNeighbours;
+    //$$
+    //$$ @NotNull
+    //$$ @Override
+    //$$ public ChunkRenderer[] getRenderChunkNeighbours() {
+    //$$     if (_renderChunkNeighbours == null) {
+    //$$         try {
+    //$$             @SuppressWarnings("JavaReflectionMemberAccess")
+    //$$             Field field = ChunkRenderer.class.getDeclaredField("renderChunkNeighboursValid");
+    //$$             field.setAccessible(true);
+    //$$             _renderChunkNeighbours = (ChunkRenderer[]) field.get(this);
+    //$$         } catch (IllegalAccessException | NoSuchFieldException e) {
+    //$$             throw new RuntimeException(e);
+    //$$         }
+    //$$     }
+    //$$     return _renderChunkNeighbours;
+    //$$ }
+    //$$
+    //$$ private static Field _renderChunkNeighboursUpdated;
+    //$$ private static Field renderChunkNeighboursUpdated() {
+    //$$     if (_renderChunkNeighboursUpdated == null) {
+    //$$         try {
+    //$$             @SuppressWarnings("JavaReflectionMemberAccess")
+    //$$             Field field = ChunkRenderer.class.getDeclaredField("renderChunkNeighboursUpated");
+    //$$             field.setAccessible(true);
+    //$$             _renderChunkNeighboursUpdated = field;
+    //$$         } catch (NoSuchFieldException e) {
+    //$$             throw new RuntimeException(e);
+    //$$         }
+    //$$     }
+    //$$     return _renderChunkNeighboursUpdated;
+    //$$ }
+    //$$
+    //$$ @Override
+    //$$ public void setRenderChunkNeighboursUpdated(boolean renderChunkNeighboursUpdated) {
+    //$$     try {
+    //$$         renderChunkNeighboursUpdated().set(this, renderChunkNeighboursUpdated);
+    //$$     } catch (IllegalAccessException e) {
+    //$$         throw new RuntimeException(e);
+    //$$     }
+    //$$ }
+    //$$
+    //$$ @Override
+    //$$ public boolean getRenderChunkNeighboursUpdated() {
+    //$$     try {
+    //$$         return (boolean) renderChunkNeighboursUpdated().get(this);
+    //$$     } catch (IllegalAccessException e) {
+    //$$         throw new RuntimeException(e);
+    //$$     }
+    //$$ }
+    //#else
     @Dynamic
-    @Accessor(value = "renderChunkNeighboursValid", remap = false)
-    RenderChunk[] getRenderChunkNeighbours();
+    @Shadow(remap = false)
+    private RenderChunk[] renderChunkNeighboursValid;
 
     @Dynamic
-    @Accessor(value = "renderChunkNeighboursUpated", remap = false)
-    boolean getRenderChunkNeighboursUpdated();
-    @Dynamic
-    @Accessor(value = "renderChunkNeighboursUpated", remap = false)
-    void setRenderChunkNeighboursUpdated(boolean updated);
+    @Shadow(remap = false)
+    private boolean renderChunkNeighboursUpated;
+
+    @NotNull
+    @Override
+    public RenderChunk[] getRenderChunkNeighbours() {
+        return renderChunkNeighboursValid;
+    }
+
+    @Override
+    public void setRenderChunkNeighboursUpdated(boolean renderChunkNeighboursUpdated) {
+        this.renderChunkNeighboursUpated = renderChunkNeighboursUpdated;
+    }
+
+    @Override
+    public boolean getRenderChunkNeighboursUpdated() {
+        return renderChunkNeighboursUpated;
+    }
+    //#endif
 }
